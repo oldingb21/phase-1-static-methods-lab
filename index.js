f724025cf1375d1aaca1405a2d2f7a3e63b5546a
Brenden Olding <oldingb21@gmail.com>
2023-06-05 02:23:12 -0400

    lab complete, first real use of a switch statement

--- index.js
+++ index.js
@@ -1,3 +1,79 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() +string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^a-zA-Z0-9 '-]/g,"")
+  }
+
+  static titleize(string) {
+    let stringArray = string.split(" ");
+    //write an array method that capitalizes the first letter of words in 
+    //a string excluding the following words:
+    //the, a, an, but, of, and, for, at, by, and from
+    let capStringArray = [];
+    stringArray.forEach((word) => {
+      switch (word) {
+        case 'the':
+          if(stringArray[0] === word) {
+            capStringArray.push(word.charAt(0).toUpperCase()+word.slice(1))
+          } else capStringArray.push(word);
+          break;
+        case 'a':
+          if(stringArray[0] === word) {
+            capStringArray.push(word.charAt(0).toUpperCase()+word.slice(1))
+          } else capStringArray.push(word);
+          break;
+        case 'an':
+          if(stringArray[0] === word) {
+            capStringArray.push(word.charAt(0).toUpperCase()+word.slice(1))
+          } else capStringArray.push(word);
+          break;
+        case 'but':
+          if(stringArray[0] === word) {
+            capStringArray.push(word.charAt(0).toUpperCase()+word.slice(1))
+          } else capStringArray.push(word);
+          break;
+        case 'of':
+          if(stringArray[0] === word) {
+            capStringArray.push(word.charAt(0).toUpperCase()+word.slice(1))
+          } else capStringArray.push(word);
+          break;
+        case 'and':
+          if(stringArray[0] === word) {
+            capStringArray.push(word.charAt(0).toUpperCase()+word.slice(1))
+          } else capStringArray.push(word);
+          break;
+        case 'for':
+          if(stringArray[0] === word) {
+            capStringArray.push(word.charAt(0).toUpperCase()+word.slice(1))
+          } else capStringArray.push(word);
+          break;
+        case 'at':
+          if(stringArray[0] === word) {
+            capStringArray.push(word.charAt(0).toUpperCase()+word.slice(1))
+          } else capStringArray.push(word);
+          break;
+        case 'by':
+          if(stringArray[0] === word) {
+            capStringArray.push(word.charAt(0).toUpperCase()+word.slice(1))
+          } else capStringArray.push(word);
+          break;
+        case 'from':
+          if(stringArray[0] === word) {
+            capStringArray.push(word.charAt(0).toUpperCase()+word.slice(1))
+          } else capStringArray.push(word);
+          break;
+        default:
+          capStringArray.push(word.charAt(0).toUpperCase()+word.slice(1))
+      }
+    })
+    /*stringArray.map(word => {
+      if (word.toLowerCase() !== "the" || "a" || "an" || "but" || "of" || "for" || "and" || "at" || "by" || "from") {
+        return word.charAt(0).toUpperCase()+word.slice(1)
+      } else return word;*/
+    //console.log(capStringArray)
+    return capStringArray.join(" ")
+  }
 }
